d8adab45734c9314e7d03e9e5237819d6630af60
Dmitry Kharlamov <newdimaak@gmail.com>
2019-08-11 17:05:49 +0300

    delete unused

--- Base/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register('_autoload', true, false);
-function _autoload($className)
-{
-  $parts = explode('_', $className);
-  $file = implode(DIRECTORY_SEPARATOR, $parts) . '.php';
-  include $file;
-}
